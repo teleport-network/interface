de05c3e8216ca5017ee2fdc2a3b214ae4ec5c179
Frank Wei <frank.wei@davionlabs.com>
2022-10-17 12:25:34 +0800

    refactor(farm): rm redundant code and comments in `PoolCard`

--- packages/ui/src/pages/Farm/PoolCard.tsx
+++ packages/ui/src/pages/Farm/PoolCard.tsx
@@ -6,22 +6,14 @@ import { ReactComponent as ArrowUp } from 'assets/svg/action/arrowUp.svg'
 import { ReactComponent as QuestionIcon } from 'assets/svg/action/question.svg'
 import { ReactComponent as RemoveIcon } from 'assets/svg/minus.svg'
 import { ButtonPrimary } from 'components/Button'
-// import { BIG_INT_SECONDS_IN_WEEK } from '../../constants'
-// import { AutoColumn } from 'components/Column'
 import DoubleCurrencyLogo from 'components/DoubleLogo'
 import ClaimRewardModal from 'components/masterchef/ClaimRewardModal'
-// import { Break } from 'components/earn/styled'
-// import { RowBetween } from 'components/Row'
 import StakingModal from 'components/masterchef/StakingModal'
 import UnstakingModal from 'components/masterchef/UnstakingModal'
-// import { Chef } from 'constants/farm/chef.enum'
-// import { Chef } from 'constants/farm/chef.enum'
-import { CHAINID_TO_FARMING_CONFIG, LiquidityAsset } from 'constants/farming.config'
+import { LiquidityAsset } from 'constants/farming.config'
 import { UNI } from 'constants/index'
 import { useActiveWeb3React } from 'hooks'
 import { useChefContractForCurrentChain } from 'hooks/farm/useChefContract'
-// import { useChefContract } from 'hooks/farm/useChefContract'
-// import { useChefPositions } from 'hooks/farm/useChefPositions'
 import { ChefStakingInfo } from 'hooks/farm/useChefStakingInfo'
 import { useChefPoolAPR } from 'hooks/farm/useFarmAPR'
 import { ApprovalState, useApproveCallback } from 'hooks/useApproveCallback'
@@ -32,14 +24,9 @@ import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import useUSDCPrice from 'utils/useUSDCPrice'
 
-// import { currencyId } from '../../utils/currencyId'
-// import { unwrappedToken } from '../../utils/wrappedCurrency'
 import { useTotalSupply } from '../../data/TotalSupply'
-// import { StakingInfo } from '../../state/stake/hooks'
 import { useColor } from '../../hooks/useColor'
 import { TYPE } from '../../theme'
-// import { Token } from '@teleswap/sdk'
-// import { useMasterChefPoolInfo } from 'hooks/farm/useMasterChefPoolInfo'
 
 const HelpTextToolTip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
@@ -177,14 +164,8 @@ const StakingColumnTitle = ({ children }: { children: React.ReactNode }) => (
 
 export default function PoolCard({ pid, stakingInfo }: { pid: number; stakingInfo: ChefStakingInfo }) {
   const { chainId } = useActiveWeb3React()
-  const farmingConfig = CHAINID_TO_FARMING_CONFIG[chainId || 420]
   const mchefContract = useChefContractForCurrentChain()
-  // const masterChef = useMasterChef(Chef.MINICHEF)
-  // const positions = useChefPositions(mchefContract, undefined, chainId)
   const history = useHistory()
-  // const poolInfos = useMasterChefPoolInfo(farmingConfig?.chefType || Chef.MINICHEF)
-  // const token0 = stakingInfo.tokens[0]
-  // const token1 = stakingInfo.tokens[1]
 
   const currency0: Token | undefined = (stakingInfo.stakingAsset as LiquidityAsset).tokenA
   const currency1: Token | undefined = (stakingInfo.stakingAsset as LiquidityAsset).tokenB
@@ -213,22 +194,6 @@ export default function PoolCard({ pid, stakingInfo }: { pid: number; stakingInf
     }
   }, [pid, stakingTokenPair, totalSupplyOfStakingToken, stakingTokenPairStatus])
 
-  // // let returnOverMonth: Percent = new Percent('0')
-  // let valueOfTotalStakedAmountInWETH: TokenAmount | undefined
-  // if (totalSupplyOfStakingToken && stakingTokenPair) {
-  //   // take the total amount of LP tokens staked, multiply by ETH value of all LP tokens, divide by all LP tokens
-  //   valueOfTotalStakedAmountInWETH = new TokenAmount(
-  //     WETH,
-  //     JSBI.divide(
-  //       JSBI.multiply(
-  //         JSBI.multiply(stakingInfo.totalStakedAmount.raw, stakingTokenPair.reserveOf(WETH).raw),
-  //         JSBI.BigInt(2) // this is b/c the value of LP shares are ~double the value of the WETH they entitle owner to
-  //       ),
-  //       totalSupplyOfStakingToken.raw
-  //     )
-  //   )
-  // }
-
   const isStaking = true
   const rewardToken = UNI[chainId || 420]
   const [isMobileActionExpanded, setMobileActionExpansion] = useState(false)
@@ -245,12 +210,11 @@ export default function PoolCard({ pid, stakingInfo }: { pid: number; stakingInf
     stakingTokenPair,
     new TokenAmount(stakingInfo.stakingToken, stakingInfo.stakedAmount.raw || '0')
   )
-  const poolInfo = farmingConfig?.pools[pid]
 
   const StakeManagementPanel = ({ isMobile, isHideInMobile }: { isMobile: boolean; isHideInMobile?: boolean }) => {
     return (
       <StakingColumn isMobile={isMobile} isHideInMobile={isHideInMobile}>
-        <StakingColumnTitle>Staked {poolInfo?.stakingAsset.isLpToken ? 'LP' : 'Token'}</StakingColumnTitle>
+        <StakingColumnTitle>Staked {stakingInfo.stakingAsset.isLpToken ? 'LP' : 'Token'}</StakingColumnTitle>
         <TYPE.white fontSize="1.2rem" marginRight="1.5rem">
           {stakingInfo.stakedAmount.toSignificant(6)}
         </TYPE.white>
@@ -344,22 +308,22 @@ This amount is already included in all APR calculations for the farm`}
       <TopSection>
         <DoubleCurrencyLogo currency0={currency0} currency1={currency1} size={24} />
         <TYPE.white fontWeight={600} fontSize={18} style={{ marginLeft: '0.26rem' }} width="12rem">
-          {poolInfo?.stakingAsset.name}
+          {stakingInfo.stakingAsset.name}
         </TYPE.white>
-        {poolInfo?.stakingAsset.isLpToken && (
+        {stakingInfo.stakingAsset.isLpToken && (
           <TYPE.green01
             marginLeft={isMobile ? 'auto' : 32}
             fontSize={14}
             onClick={() =>
               history.push(
                 `/add/${currency0?.address}/${currency1?.address}/${
-                  poolInfo.stakingAsset.isLpToken && poolInfo.stakingAsset.isStable
+                  stakingInfo.stakingAsset.isLpToken && stakingInfo.stakingAsset.isStable
                 }`
               )
             }
             style={{ cursor: 'pointer' }}
           >
-            Get {poolInfo?.stakingAsset.name} LP
+            Get {stakingInfo.stakingAsset.name} LP
           </TYPE.green01>
         )}
       </TopSection>
@@ -386,7 +350,6 @@ This amount is already included in all APR calculations for the farm`}
             style={{ cursor: 'pointer', display: 'flex' }}
           >
             Details
-            {/* {!isMobileActionExpanded ? <ArrowDown width="0.9rem" /> : <ArrowUp width="0.9rem" />} */}
             {!isMobileActionExpanded ? <StyledArrowDown /> : <StyledArrowUp />}
           </TYPE.green01>
         </StakingColumn>
@@ -398,26 +361,6 @@ This amount is already included in all APR calculations for the farm`}
           <MultiplierStatus />
         </MobilePoolDetailSection>
       )}
-      {/* {isStaking && (
-        <>
-          <BottomSection showBackground={true}>
-            <TYPE.black color={'white'} fontWeight={500}>
-              <span>You earned</span>
-            </TYPE.black>
-
-            <TYPE.black style={{ textAlign: 'right' }} color={'white'} fontWeight={500}>
-              {stakingInfo
-                ? stakingInfo.active
-                  ? `${stakingInfo.rewardRate
-                      ?.multiply(BIG_INT_SECONDS_IN_WEEK)
-                      ?.toSignificant(4, { groupSeparator: ',' })} UNI / week`
-                  : '0 UNI / week'
-                : '-'}
-              810.1919 UNI
-            </TYPE.black>
-          </BottomSection>
-        </>
-      )} */}
       <>
         <StakingModal
           stakingInfo={stakingInfo}
