6f4035f6e158ea2d72d17a3e24fdfc9fa5fe784c
pop <906792895@qq.com>
2022-10-28 18:10:26 +0800

    fix: delete WETH

--- packages/ui/src/components/CurrencyLogo/index.tsx
+++ packages/ui/src/components/CurrencyLogo/index.tsx
@@ -1,4 +1,4 @@
-import { Currency, ETHER, Token, WETH } from '@teleswap/sdk'
+import { Currency, ETHER, Token } from '@teleswap/sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
